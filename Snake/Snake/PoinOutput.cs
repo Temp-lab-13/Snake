2d5a270029278c437143f881f9772ec48bcc44ac
Temp <tap-kro-tap@protonmail.com>
2021-07-05 14:17:56 +0700

    Подготовка

---
+++ Snake/Snake/PoinOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class PoinOutput
+    {
+        public int x;
+        public int y;
+        public char symbol;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PoinOutput p1 = new PoinOutput();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symbol = '*';
+            p1.Draw();
+
+            PoinOutput p2 = new PoinOutput();
+            p2.x = 4;
+            p2.y = 5;
+            p2.symbol = '#';
+            p2.Draw();
+        }
+    }
+}
