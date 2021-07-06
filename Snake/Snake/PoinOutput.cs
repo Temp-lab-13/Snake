9b8bc069c05d0eacefe140e8a32ea1c8f0f203bf
Temp <tap-kro-tap@protonmail.com>
2021-07-06 14:09:55 +0700

    создание змейки

--- Snake/Snake/PoinOutput.cs
+++ Snake/Snake/PoinOutput.cs
@@ -6,14 +6,54 @@ namespace Snake
 {
     class PoinOutput
     {
+
         public int x;
         public int y;
         public char symbol;
+
+        public PoinOutput(int X, int Y, char Symbol)
+        {
+            x = X;
+            y = Y;
+            symbol = Symbol;
+        }
+
+        public PoinOutput(PoinOutput p)
+        {
+            x = p.x;
+            y = p.y;
+            symbol = p.symbol;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x = x - offset;
+            }
+            else if(direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if(direction == Direction.UP)
+            {
+                y = y - offset;
+            }
+            else if(direction == Direction.Down)
+            {
+                y = y - offset;
+            }
+        }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(symbol);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + symbol;
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,17 +6,12 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            PoinOutput p1 = new PoinOutput();
-            p1.x = 1;
-            p1.y = 3;
-            p1.symbol = '*';
+            PoinOutput p1 = new PoinOutput(1, 3, '*');
             p1.Draw();
+            Snaake snake = new Snaake(p, 5, Direction.RIGHT);
+            snake.Draw();
 
-            PoinOutput p2 = new PoinOutput();
-            p2.x = 4;
-            p2.y = 5;
-            p2.symbol = '#';
-            p2.Draw();
+       
         }
     }
 }
