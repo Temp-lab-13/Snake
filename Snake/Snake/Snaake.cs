ffe7edcca0857b92fdc9975305b2e0496bdf6df0
Temp <tap-kro-tap@protonmail.com>
2021-07-06 15:41:54 +0700

    перенос класса snace

---
+++ Snake/Snake/Snaake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Snaake : Figure
+    {
+        public Snaake(PoinOutput tail, int lenght, Direction direction)
+        {
+            pList = new List<PoinOutput>();
+            for (int i = 0; i < lenght; i++)
+            {
+                PoinOutput p = new PoinOutput(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+    }
+}
