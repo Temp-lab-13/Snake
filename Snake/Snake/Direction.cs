6bc53309951e995aee71f0bd1056cbe6f5e5b0c5
Temp <tap-kro-tap@protonmail.com>
2021-07-06 15:45:30 +0700

    Перенос класса Direction

---
+++ Snake/Snake/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    enum Direction
+    {
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+    }
+}
