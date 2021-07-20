3ba4674fd9bbd893583642fa7f375707df21465a
Temp <tap-kro-tap@protonmail.com>
2021-07-20 19:08:15 +0700

    Создание класса GameOver. Финал.

---
+++ Snake/Snake/GameOver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class GameOver
+    {
+        public static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("G A M E   O V E R !", xOffset + 5, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+
+
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -46,27 +46,9 @@ namespace Snake
                     snake.HandKey(key.Key);
                 }
             }
-            WriteGameOver();
-            Console.ReadLine();
 
-            static void WriteGameOver()
-            {
-                int xOffset = 25;
-                int yOffset = 8;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.SetCursorPosition(xOffset, yOffset++);
-                WriteText("============================", xOffset, yOffset++);
-                WriteText("G A M E   O V E R !", xOffset + 5, yOffset++);
-                WriteText("============================", xOffset, yOffset++);
-                
-            }
-           
-            static void WriteText(String text, int xOffset, int yOffset)
-            {
-                Console.SetCursorPosition(xOffset, yOffset);
-                Console.WriteLine(text);
-            }
-            
+            GameOver.WriteGameOver();
+            Console.ReadLine();
         }
     }
 }
